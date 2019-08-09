b8cdeb41417bafc8f1a53e582d5e2d0661642112
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-09 16:12:35 -0400

    adder function for shiggles

--- fe/src/App.test.js
+++ fe/src/App.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from './App';
+import adder from './adder';
 
 
 describe('testing default components rendered in <App />', () => {
@@ -10,5 +11,6 @@ describe('testing default components rendered in <App />', () => {
 
   it('renders without crashing', () => {
     expect(getByTestId(/^App$/)).toBeInTheDocument();
+    expect(adder(2, 5)).toBe(7);
   });
 })
